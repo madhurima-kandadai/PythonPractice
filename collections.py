0cc8561ea43e1ac054782835381253cb69249731
Madhurima Kandadai <madhurima_kandadai@epam.com>
2017-03-21 15:27:01 +0530

    changes

--- collections.py
+++ collections.py
@@ -1,14 +1,2 @@
-count = 0
-
-def show_count():
-	print(count)
-	
-
-def set_count(c):
-	global count
-	count = c
-	
-	
-def increment_count():
-	global count
-	count = count + 1
+def minmax(items):
+	return min(items), max(items)
